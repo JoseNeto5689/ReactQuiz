0a8f292b461aebc362ff34e600f1c5edba570498
JoseNeto5689 <jose.sicupiraneto@gmail.com>
2021-10-16 10:10:53 -0300

    Botão consertado

--- components/optionBox.js
+++ components/optionBox.js
@@ -1,8 +1,12 @@
 import React from "react";
 import Option from "./option";
-import { View, StyleSheet } from "react-native";
+import { Text, View, StyleSheet, TouchableHighlight } from "react-native";
+import { Inter_400Regular ,Inter_900Black, useFonts } from "@expo-google-fonts/inter";
+import { OpenSans_400Regular } from "@expo-google-fonts/open-sans";
+import AppLoading from 'expo-app-loading';
 
 export default function({ option1, option2, option3, option4, op1,op2,op3,op4, navigation}){
+  
 const verify = (num) => {
   if(num === 0){
     navigation.navigate("Error")
@@ -12,20 +16,42 @@ const verify = (num) => {
   }
 }
 
-    return (
+return <>
     <View style = { styles.container } >
-      <Option content = { option1 } action = { () => {verify(op1)} } />
-      <Option content = { option2 } action = { () => {verify(op2)} }/>
-      <Option content = { option3 } action = { () => {verify(op3)} }/>
-      <Option content = { option4 } action = { () => {verify(op4)} }/>
+      <TouchableHighlight style = { styles.optionBox } underlayColor = { "#D44426" } onPress = { () => {verify(op1)} } >
+              <Text style = { styles.optionText } > { option1 } </Text>
+      </TouchableHighlight>
+      <TouchableHighlight style = { styles.optionBox } underlayColor = { "#D44426" } onPress = { () => {verify(op2)} } >
+              <Text style = { styles.optionText } > { option2 } </Text>
+      </TouchableHighlight>
+      <TouchableHighlight style = { styles.optionBox } underlayColor = { "#D44426" } onPress = { () => {verify(op3)} } >
+              <Text style = { styles.optionText } > { option3 } </Text>
+      </TouchableHighlight>
+      <TouchableHighlight style = { styles.optionBox } underlayColor = { "#D44426" } onPress = { () => {verify(op4)} } >
+              <Text style = { styles.optionText } > { option4 } </Text>
+      </TouchableHighlight>
     </View>
-    )
+</>
 }
-
 const styles = StyleSheet.create({
     container: {
-      //backgroundColor: "blue",
       alignItems: 'center',
       marginTop: 20,
-    }
+    },
+    optionBox: {
+      height: 60,
+      width: 270,
+      backgroundColor: "orange",
+      borderColor: "#D44426",
+      borderWidth: 6,
+      borderRadius: 5,
+      alignItems: "center",
+      justifyContent: "center",
+      marginVertical: 20,
+  },
+  optionText: {
+      fontSize: 20,
+      color: "#201F1F",
+      fontWeight: "bold",
+  }
   });
--- components/questionFull.js
+++ components/questionFull.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, StatusBar } from 'react-native';
+import { Text, View, StyleSheet, TouchableHighlight, StatusBar } from "react-native";
 import Question from './question';
 import OptionBox from "./optionBox"
 import Timer from "./timer"
@@ -12,16 +12,37 @@ export default function({ question, options, timer, navigation, answer }) {
     if (seconds > 0) {
       time = setInterval(() => setSeconds(seconds - 1), 1000)
       return function cleanUp() {
-        clearTimeout(time)
-        time = null
+        clearInterval(time)
     }
     }})
+
+    const verify = (num) => {
+      if(num === 0){
+        navigation.navigate("Error")
+      }
+      if(num === 1){
+      navigation.navigate("WinScreen")
+      }
+    }
   if(seconds !== 0){
     return <>
       <StatusBar style="auto" />
       <View style = { { backgroundColor: "#EBEBEB", height: "100%" } } >
         <Question {...question}/>
-        <OptionBox {...options} navigation = { navigation } { ...answer } />
+        <View style = { styles.container } >
+          <TouchableHighlight style = { styles.optionBox } underlayColor = { "#D44426" } onPress = { () => { clearInterval(time) ;verify(answer.op1)} } >
+                  <Text style = { styles.optionText } > { options.option1 } </Text>
+          </TouchableHighlight>
+          <TouchableHighlight style = { styles.optionBox } underlayColor = { "#D44426" } onPress = { () => { clearInterval(time) ;verify(answer.op2)} } >
+                  <Text style = { styles.optionText } > { options.option2 } </Text>
+          </TouchableHighlight>
+          <TouchableHighlight style = { styles.optionBox } underlayColor = { "#D44426" } onPress = { () => {clearInterval(time);verify(answer.op3)} } >
+                  <Text style = { styles.optionText } > { options.option3 } </Text>
+          </TouchableHighlight>
+          <TouchableHighlight style = { styles.optionBox } underlayColor = { "#D44426" } onPress = { () => {clearInterval(time);verify(answer.op4)} } >
+                  <Text style = { styles.optionText } > { options.option4 } </Text>
+          </TouchableHighlight>
+        </View>
         <Timer time = {seconds}/>
       </View>
     </> }
@@ -31,3 +52,26 @@ export default function({ question, options, timer, navigation, answer }) {
     return <Error navigation = { navigation } />
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    alignItems: 'center',
+    marginTop: 20,
+  },
+  optionBox: {
+    height: 60,
+    width: 270,
+    backgroundColor: "orange",
+    borderColor: "#D44426",
+    borderWidth: 6,
+    borderRadius: 5,
+    alignItems: "center",
+    justifyContent: "center",
+    marginVertical: 20,
+},
+optionText: {
+    fontSize: 20,
+    color: "#201F1F",
+    fontWeight: "bold",
+}
+});
