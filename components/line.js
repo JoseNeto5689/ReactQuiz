317657caeba9811f92e5830b3389b3b2bfc6d122
JoseNeto5689 <jose.sicupiraneto@gmail.com>
2021-10-23 17:27:40 -0300

    Line.js removido

--- components/line.js
+++
@@ -1,6 +0,0 @@
-import React from "react"
-import { View } from "react-native"
-
-export default function Line(){
-    return <View style = {{ height: 4, width: "98%", backgroundColor: 'black' }} />
-}
